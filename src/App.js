e7d0f0391d40a5784df721c417b7b14aaa4f6c46
Ilyas <79158764+mr-ceri-mrum@users.noreply.github.com>
2025-04-04 16:49:10 +0500

    Add App.js with routing

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/layout/Layout';
+import HomePage from './pages/HomePage';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          {/* Additional routes will be added here */}
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
